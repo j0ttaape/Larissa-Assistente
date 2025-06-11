f6321952b7c6c31441fe6727d704994efb4d16f1
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-11 18:18:15 -0300

    primeiros passos
    inicialização do projeto

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+
+
+export function ApresentarLarissa(){
+    console.log(
+        `--------------------------------------------------------------------------
+        Olá me chamo Larissa, é um prazer te conhecer. Com o que iremos trabalhar hoje?
+        `
+    )
+}

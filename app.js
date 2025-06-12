3a2f7f4dea37d6eabc66aaa479ddb8a5bfe8135b
j0ttaape <joaopedrotrajano59@gmail.com>
2025-06-12 10:24:02 -0300

    consolelogs
    criando a interface

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 import prompt from 'prompt-sync'
+import { ApresentarLarissa, esperar, ApresentarContasMatematicas } from "./funcoes.js";
+
+
+console.log(ApresentarLarissa());
+
+await esperar(3000);
+
+console.log(ApresentarContasMatematicas());
+
--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,39 @@
+import prompt from 'prompt-sync';
+let ler = prompt();
 
+// Apresentação da assistente
+export function ApresentarLarissa() {
+    return `--------------------------------------------------------------------------
+👩‍🏫 Olá! Me chamo Larissa, é um prazer te conhecer.
+Com o que iremos trabalhar hoje?
+--------------------------------------------------------------------------`;
+}
 
-export function ApresentarLarissa(){
-    console.log(
-        `--------------------------------------------------------------------------
-        Olá me chamo Larissa, é um prazer te conhecer. Com o que iremos trabalhar hoje?
-        `
-    )
+// Mostrar as contas disponíveis
+export function ApresentarContasMatematicas() {
+    return `--------------------------------------------------------------------------
+📚 Contas Matemáticas que eu posso resolver:
+
+➕ Adição.............. 
+➖ Subtração........... 
+✖️ Multiplicação....... 
+➗ Divisão..............
+🧮 Parênteses.......... 
+📐 Potenciação......... 
+√ Raiz quadrada........ 
+% Porcentagem.......... 
+
+Digite sua conta e eu resolvo rapidinho! 😄
+--------------------------------------------------------------------------`;
+}
+
+// Esperar X milissegundos
+export function esperar(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//Pedindo a conta
+export function Pedindoaconta(){
+    
+
 }
